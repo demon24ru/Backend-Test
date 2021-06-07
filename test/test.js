c1ee7df1c248297eec5bcd807dca6e603656f32b
demon-dem <ittek@inbox.ru>
2021-06-07 14:00:24 +0700

    init 3

--- test/test.js
+++ test/test.js
@@ -2,9 +2,17 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('App', function() {
-  it('has the default page', function(done) {
+
+  let auth;
+  it('has the auth', function(done) {
     request(app)
-      .get('/')
-      .expect(/Welcome to Express/, done);
+        .get('/auth?user=test')
+        .expect(200)
+        .then(res => {
+          auth = res.header.authorization;
+          done();
+        })
+        .catch(err => done(err));
   });
-}); 
+
+});
